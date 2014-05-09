7d91d45791ca56dc79043772d377e7e8e083be64
clauper <clauper@postmail.ch>
2014-05-09 15:40:49 +0100

    plot 4 r skript and picture

---
+++ plot4.R
@@ -0,0 +1,33 @@
+
+# this r skript will produce plot 4
+
+# set your working directory to the folder where the file below is stored
+
+# import whole data
+d.project1 <- read.table(file = "household_power_consumption.txt", sep=";", na.strings="?", header=T)
+
+
+# subset the data with the two dates used
+s.project1 <- d.project1[(d.project1$Date=="1/2/2007")|(d.project1$Date=="2/2/2007"),]
+
+# convert column time, imported as character, to date
+s.project1$Time <- strptime(paste(s.project1$Date, s.project1$Time, sep=" "), format="%d/%m/%Y %H:%M:%S")
+# (this is not used) s.project1$Date <- strptime(s.project1$Date, format="%d/%m/%Y")
+
+
+# plot 4
+
+png(filename="plot4.png", width=480, height=480)
+par(mfrow=c(2,2),mar=c(4,4,1,0.5))
+plot (s.project1$Time,s.project1$Global_active_power,xlab="",ylab="Global Active Power", type="l")
+
+plot (s.project1$Time,s.project1$Voltage,ylab="Voltage", xlab="datetime", type="l")
+
+plot(s.project1$Time,s.project1$Sub_metering_1,xlab="",ylab="Energy sub metering", type="l")
+lines(s.project1$Time,s.project1$Sub_metering_2, type="l", col="red")
+lines(s.project1$Time,s.project1$Sub_metering_3, type="l", col="blue")
+legend("topright",paste("Sub_metering_",c(1:3),sep=""),lty=1,bty="n",col=c("black", "red", "blue"),bg="white")
+
+plot (s.project1$Time,s.project1$Global_reactive_power,ylab="Global_reactive_power", xlab="datetime",type="l")
+
+dev.off()
